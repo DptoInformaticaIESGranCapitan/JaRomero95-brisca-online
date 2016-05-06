530a4e2ab5a3750e12b48eb7c12b180fc6ea1aea
Romero <Romero-PC>
2016-05-06 18:20:50 +0200

    index funciona

--- wreck-them/src/WreckThemBundle/Controller/DefaultController.php
+++ wreck-them/src/WreckThemBundle/Controller/DefaultController.php
@@ -3,15 +3,11 @@
 namespace WreckThemBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
     public function indexAction()
     {
-        return $this->render('WreckThemBundle:Default:index.html.twig');
+        return $this->render('WreckThemBundle::index.html.twig');
     }
 }
